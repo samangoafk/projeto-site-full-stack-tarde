006989b328805c01bab62167ea0a5dc287920029
Sergio Murilo <sergiomurilo2008fr@gmail.com>
2026-06-30 14:36:37 -0300

    Atualiza e corrige erros de ortografia dos arquivos index.html e script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 function mostrarMensagem(){
     document.getElementById("mensagem").innerHTML =
-     "Parabéns! Seu Projeto está funcionando corretamento.";}
+     "Parabéns! Seu Projeto está funcionando corretamente.";}
 
      window.onload = function() {
     alert("Bem-vindo ao Site Full Stack Tarde!");
